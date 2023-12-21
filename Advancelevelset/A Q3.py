ee11929ec4b5670f7993fba2cc397d26e4eba8a4
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2023-12-21 05:59:11 +0530

    Update A Q3.py

--- Advancelevelset/A Q3.py
+++ Advancelevelset/A Q3.py
@@ -1,1 +1,21 @@
+def JTOI(data):
+    try : 
+        #write content in file
+        file = open("WORDS.txt","w" )
+        file.write(data)
+        file.close()
 
+        #read
+        file = open("WORDS.txt","r")
+        content  = file.read()
+        res = "" 
+        content = content.replace("j","i")
+        content  = content.replace("J","I")
+        res += content 
+        return res  
+
+    except FileNotFoundError:
+        return "file not found"
+data = input()
+print(JTOI(data))
+    
