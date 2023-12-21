c7c4aecfe7d5f556e661bd606355eb954816b562
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2023-12-21 05:51:21 +0530

    Update A Q2.py

--- Advancelevelset/A Q2.py
+++ Advancelevelset/A Q2.py
@@ -2,23 +2,11 @@ def test(n):
     return [n + 2 * i for i in range(n)]
 n = 7
 print(test(n))
-"""
-
-"""
 open_file = open("demo.txt","r")
-
 count  = 0
-
 text = open_file.read()
-
 list = text.split("\n")
-
 for x in list:
-
     if x:
-
         count += 1
-
-
-
 print("Total number of lines in the text file are:", count )
