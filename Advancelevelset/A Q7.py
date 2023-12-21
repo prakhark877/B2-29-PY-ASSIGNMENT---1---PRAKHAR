98528ccf6ce48ec654452d412ef9231098f1ebc5
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2023-12-21 05:54:12 +0530

    Update A Q7.py

--- Advancelevelset/A Q7.py
+++ Advancelevelset/A Q7.py
@@ -1,1 +1,13 @@
-
+def student(name,subject):
+         """
+         d  = {}
+         for n,s in zip(name,subject):
+                 if n not in d :
+                     d[n] = s 
+        return d
+        """
+        return {n:s for n,s in zip( name, subject)}
+     
+name = input().split()
+subject = input().split()
+print(student(name, subject))
