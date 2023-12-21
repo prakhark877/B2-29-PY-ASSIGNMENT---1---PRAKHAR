aec5595ea2833a22681b455e6bb83920763a8033
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2023-12-21 05:53:12 +0530

    Update A Q8.py

--- Advancelevelset/A Q8.py
+++ Advancelevelset/A Q8.py
@@ -1,1 +1,8 @@
-
+def user(x):
+    new = x.split('0')
+    l = [ e for e in new if len(e) !=0]
+    f = {"first_name" :  l[0] , "last_name":l[1] , "id":l[2]}
+    return f 
+ 
+x = input()
+print(user(x))
