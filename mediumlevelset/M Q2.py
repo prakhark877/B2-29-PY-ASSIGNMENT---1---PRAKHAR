743736eb2b208674eafaa6aae9370d46cb890dca
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2023-12-27 07:09:22 +0530

    Added all the files.

---
+++ mediumlevelset/M Q2.py
@@ -0,0 +1,6 @@
+def return_l(s) :
+    t = set(s)
+    return t
+s = [1, 2, 2, 3, 4, 4, 5, 5]
+res = list(return_l(s))
+print(res)
---
+++ mediumlevelset/M QXIII.py
@@ -0,0 +1,4 @@
+check = lambda x, char : x.startswith(char)
+s = "Hello, World!"
+k = "H"
+print(check(s,k))
