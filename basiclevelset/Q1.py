ad2e0d6ec53a052fbd03cd03816d906bf83c2701
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2023-12-27 06:55:51 +0530

    Create Q1.py
    Added all the files.

---
+++ basiclevelset/Q1.py
@@ -0,0 +1,10 @@
+def func(s) :
+    if s%3==0 :
+        print('Brudite')
+    if s%5==0 :
+        print('Python Training')
+    if s%3==0 and s%5==0 :
+        print('Brudite - Python Training')
+
+s = int(input('Enter a number : '))
+func(s)
