7c7c6b4184098c75d883b0ac88337ec6b8d8fbee
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2024-01-04 07:13:09 +0530

    Update A Q4.py

--- advancelevelset/A Q4.py
+++ advancelevelset/A Q4.py
@@ -1,15 +1,15 @@
-class Shape:
+class Structure:
     def area():
         return 0
-
-class Square(Shape):
-    def __init__(self,length):
-        self.length = length
+        
+class Square(Structure):
+    def __init__(self,l):
+        self.l = l
     def area(self):
-        return self.length**2
+        return self.l**2
 
-obj_superclass = Shape
-print(f"area of shape: {obj_superclass.area()}")
-length = int(input("side of the square: "))
-obj_subclass = Square(length)
-print(f"area of square :{obj_subclass.area()}")
+obj = Structure
+print(f"area of struture is : {obj.area()}")
+l = int(input("Enter the side of the square : "))
+obj = Square(l)
+print(f"area of square :{obj.area()}")
