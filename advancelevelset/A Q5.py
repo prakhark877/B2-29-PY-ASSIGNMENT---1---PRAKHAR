5f0f1f29a55edc0379ee1032355d8399185aa2e3
Prakhar Kabra <prakhar.k.brudite@gmail.com>
2024-01-04 08:24:42 +0530

    Update A Q5.py

--- advancelevelset/A Q5.py
+++ advancelevelset/A Q5.py
@@ -1,28 +1,28 @@
-class Time:
+class Timing:
     def __init__(self,hours,minute):
-        self.hours = hours 
+        self.hours = hours
         self.minute = minute
         self.tmin = hours*60 + minute
 
-    def addTime(self, time2 ):
-        self.totalmin = self.minute  + time2.minute 
-        if (self.totalmin >= 60):
+    def addTiming(self, time2 ):
+        self.totamin = self.minute  + time2.minute
+        if (self.totamin >= 60):
 
-            self.totalhrs = self.hours  + time2.hours + self.totalmin//60
-            self.totalmin = (self.minute + time2.minute)%60
+            self.totahrs = self.hours  + time2.hours + self.totamin//60
+            self.totamin = (self.minute + time2.minute)%60
         else:
-            self.totalhrs = self.hours + time2.hours 
-            self.totalmin = self.minute +time2.minute
-            
-        self.tmin += time2.tmin 
-    def displayTime(self):
-        print( f"{self.totalhrs} hrs {self.totalmin} min" )
+            self.totahrs = self.hours + time2.hours
+            self.totamin = self.minute +time2.minute
+
+        self.tmin += time2.tmin
+    def displayTiming(self):
+        print( f"{self.totahrs} hrs {self.totamin} min" )
 
     def displayMinute(self):
-        print(f"Total minutes: { self.tmin}") 
-    
-t1= Time(12,50)
-t2 = Time(2,20)
-t1.addTime(t2)
-t1.displayTime()
-t1.displayMinute()
+        print(f"Total minutes: { self.tmin}")
+
+ta= Timing(12,50)
+tb = Timing(2,20)
+ta.addTiming(tb)
+ta.displayTiming()
+ta.displayMinute()
